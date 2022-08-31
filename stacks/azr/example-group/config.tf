faf942a8d73a3bb6533fe01bf3df26252974bc0d
Massimiliano <MassimilianoDH95@gmail.com>
2022-08-31 16:33:49 -0400

    test

--- stacks/azr/example-group/config.tf
+++ stacks/azr/example-group/config.tf
@@ -15,3 +15,13 @@ terraform {
     key                  = "states.example-group.terraform.tfstate"
   }
 }
+
+variable "client_secret" {}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = var.client_secret
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
+}
